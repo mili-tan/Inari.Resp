1df4cd6aae31d287348a12f1e950170f39180ba3
Milkey Tan <819774989@qq.com>
2021-08-11 21:12:54 +0800

    mv

---
+++ Manager/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace Inari.Resp.Manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            MessageBox.Show(e.Args.FirstOrDefault());
+        }
+    }
+}
---
+++ Manager/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Inari.Resp.Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
